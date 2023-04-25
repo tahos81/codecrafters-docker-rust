28ab6370093dafddb8c889d0f71c1566927540c8
tahos81 <tahirozpala@gmail.com>
2023-04-25 15:18:27 +0300

    Stage 3 Attempt 1

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,28 @@
 use anyhow::{Context, Result};
-use std::env;
-use std::process::{self, exit, Stdio};
+use libc::chroot;
+use tempfile::TempDir;
+
+use std::{
+    env,
+    ffi::CString,
+    fs,
+    os::unix::prelude::OsStrExt,
+    path::Path,
+    process::{self, exit, Stdio},
+};
 
 // Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
 fn main() -> Result<()> {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
+    let temp_dir = TempDir::new()?;
+    fs::create_dir_all(temp_dir.path().join("usr/local/bin/"))?;
+    fs::create_dir_all(temp_dir.path().join("dev/null"))?;
+
+    let source_path = Path::new("/usr/local/bin/docker-explorer");
+    let dest_path = temp_dir.path().join("usr/local/bin/docker-explorer");
+    fs::copy(source_path, &dest_path)?;
+
+    let c_path = CString::new(temp_dir.path().as_os_str().as_bytes()).unwrap();
+    unsafe { chroot(c_path.as_ptr()) };
 
     let args: Vec<_> = env::args().collect();
     let command = &args[3];
