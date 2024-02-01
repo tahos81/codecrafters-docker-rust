cadedf5a742b2f19e1d6e3b38acb7bfe3a78b569
tahos81 <tahirozpala@gmail.com>
2024-02-01 15:55:19 +0300

    chroot

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,25 @@
 //use std::process::Stdio;
-
 use anyhow::{Context, Result};
+use std::{env, fs, os::unix::fs::chroot, path::Path};
+use tempfile::TempDir;
 
 // Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
 fn main() -> Result<()> {
+    let temp_dir = TempDir::new()?;
+    fs::create_dir_all(temp_dir.path().join("usr/local/bin/"))?;
+    fs::create_dir_all(temp_dir.path().join("dev/null"))?;
+    let source_path = Path::new("/usr/local/bin/docker-explorer");
+    let dest_path = temp_dir.path().join("usr/local/bin/docker-explorer");
+    fs::copy(source_path, &dest_path)?;
+    chroot(temp_dir.path())?;
+    env::set_current_dir("/")?;
+
     let args: Vec<_> = std::env::args().collect();
     let command = &args[3];
     let command_args = &args[4..];
+
     let output = std::process::Command::new(command)
         .args(command_args)
-        // .stdout(Stdio::piped())
-        // .stderr(Stdio::piped())
         .output()
         .with_context(|| {
             format!(
@@ -19,14 +28,9 @@ fn main() -> Result<()> {
             )
         })?;
 
-    if output.status.success() {
-        let std_out = std::str::from_utf8(&output.stdout)?;
-        print!("{}", std_out);
-        let std_err = std::str::from_utf8(&output.stderr)?;
-        eprint!("{}", std_err);
-    } else {
-        std::process::exit(output.status.code().unwrap_or(1));
-    }
-
-    Ok(())
+    let std_out = std::str::from_utf8(&output.stdout)?;
+    print!("{}", std_out);
+    let std_err = std::str::from_utf8(&output.stderr)?;
+    eprint!("{}", std_err);
+    std::process::exit(output.status.code().unwrap_or(1));
 }
